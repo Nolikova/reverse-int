642edca01014ff8b1c8d476a13d0ff17686e2fef
Anzhelika Novikova <anzhelika.novikova93@gmail.com>
2023-01-04 21:44:27 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n)
+  let string = num.toString() 
+  let array = string.split('') 
+  let result = array.reverse()  
+  let string1 = result.join('')
+  let res = Number(string1)
+  return res;
 }
